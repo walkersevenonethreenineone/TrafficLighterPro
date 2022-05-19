6acb0cadebcc05ef11d335f58bb04ad9ee246ffe
walkersevenonethreenineone <98523965+walkersevenonethreenineone@users.noreply.github.com>
2022-05-19 13:16:17 +0300

    pupsik's code

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
-function changeColor(){
-    let redCircle = document.getElementById("red");
-    if(redCircle.style.backgroundColor == "red"){
-        redCircle.style.backgroundColor = "black";
-    }else{
-        redCircle.style.backgroundColor = "black";
-    }    
+var lightStates = {red:0,amber:1,green:2};
+var currentState = lightStates.red;
+
+document.getElementById('changeBtn').onclick=function(){
+    changeState();
+};
+
+
+function changeState()
+{
+    clear();
+  switch(currentState)
+  {
+    case lightStates.red:
+    {
+      document.getElementById("red").className ="light red";
+      currentState =  lightStates.amber;
+    }
+      break;
+    case lightStates.amber:
+    {
+      document.getElementById("amber").className ="light amber";
+      currentState = lightStates.green;
+    } break;
+     case lightStates.green:
+    {
+      document.getElementById("green").className ="light green";
+      currentState = lightStates.red;
+    } break;
+   }
+}
+
+function clear(){
+   document.getElementById("red").className ="light off";
+   document.getElementById("amber").className ="light off";
+   document.getElementById("green").className ="light off";
 }
