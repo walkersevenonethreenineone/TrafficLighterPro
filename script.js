b2546ae420738ff731d46502c2db833886a73448
walkersevenonethreenineone <98523965+walkersevenonethreenineone@users.noreply.github.com>
2022-05-19 13:12:20 +0300

    1st upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function changeColor(){
+    let redCircle = document.getElementById("red");
+    if(redCircle.style.backgroundColor == "red"){
+        redCircle.style.backgroundColor = "black";
+    }else{
+        redCircle.style.backgroundColor = "black";
+    }    
+}
