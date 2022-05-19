9695b8574982d3e0e963afb46a9b05ea6614c20d
walkersevenonethreenineone <98523965+walkersevenonethreenineone@users.noreply.github.com>
2022-05-19 13:11:47 +0300

    1st upload

---
+++ style.css
@@ -0,0 +1,25 @@
+.background{
+    background-color: gray;
+    width: 200px;
+    height: 400px;
+    padding: 20px;
+}
+
+.circle{
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    margin: 20px;
+}
+
+.red{
+    background-color: black;
+}
+
+.yellow{
+    background-color: black;
+}
+
+.green{
+    background-color: black; 
+}
